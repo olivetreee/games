642da51d90c8bd9d4583e95991b207a3ba455825
Clapinton <eric.olivetree@gmail.com>
2016-09-21 20:22:00 -0700

    Add methods for deep dup and helpers for checkmate, as well as position pieces.

--- chess/board.rb
+++ chess/board.rb
@@ -1,23 +1,29 @@
 require_relative 'null_piece'
 require_relative 'piece'
+require 'byebug'
 # require_relative 'display'
 
 class Board
-  attr_reader :grid
+  attr_accessor :grid, :all_pieces
   def self.initialize_grid
-    grid = Array.new(8) {Array.new(8) {nil}}
+    grid = Array.new(8) {Array.new(8) {NullPiece.instance}}
+  end
 
-    # NullPieces
-    (2..5).each do |row|
-      grid[row].map! {|cell| NullPiece.instance}
-    end
+  def initialize
+    @grid = self.class.initialize_grid
+    populate_grid
+    @all_pieces = []
+    store_all_pieces
 
 
-    # Regular Pieces
+  end
+
+  def populate_grid
     # Pawns
     grid[1].map!.with_index {|cell,idx| Pawn.new(:pawn,"black",self,[1,idx])}
     grid[6].map!.with_index {|cell,idx| Pawn.new(:pawn,"white",self,[6,idx])}
 
+    #Regular Pieces
     [0,7].each do |col|
       self[[0, col]] = Rook.new(:rook, "black", self, [0, col])
     end
@@ -26,30 +32,34 @@ class Board
       self[[7, col]] = Rook.new(:rook, "white", self, [7, col])
     end
 
-    # (0..0).each do |row|
-    #   grid[row].map! {|cell| Piece.new(:king,"white",self,[0,0])}
-    # end
-    #
-    # (7..7).each do |row|
-    #   grid[row].map! {|cell| Piece.new(:king,"red",self,[0,0])}
-    # end
+    [1,6].each do |col|
+      self[[0, col]] = Knight.new(:knight, "black", self, [0, col])
+    end
 
-    # #Uncomment below to fill with NullPieces for testing purposes
-    # (0..7).each do |row|
-    #   grid[row].map! {|cell| NullPiece.instance}
-    # end
+    [1,6].each do |col|
+      self[[7, col]] = Knight.new(:knight, "white", self, [7, col])
+    end
+
+    [2,5].each do |col|
+      self[[0, col]] = Bishop.new(:bishop, "black", self, [0, col])
+    end
+
+    [2,5].each do |col|
+      self[[7, col]] = Bishop.new(:bishop, "white", self, [7, col])
+    end
+
+    self[[0, 3]] = Queen.new(:queen, "black", self, [0, 3])
+    self[[7, 3]] = Queen.new(:queen, "white", self, [7, 3])
+    self[[0, 4]] = King.new(:king, "black", self, [0, 4])
+    self[[7, 4]] = King.new(:king, "white", self, [7, 4])
 
 
-
-    grid
   end
 
-  def initialize
-    @grid = self.class.initialize_grid
-
-    # # #Uncomment below to fill with NullPieces for testing purposes
-    # @grid[3][3]= Piece.new(:king,"white",self,[3,3])
-    # @grid[5][5]= Bishop.new(:bishop,"white",self,[5,5])
+  def store_all_pieces
+    (0..1).each { |i| @all_pieces << @grid[i] }
+    (6..7).each { |i| @all_pieces << @grid[i] }
+    @all_pieces.flatten!
   end
 
   def [](pos)
@@ -78,6 +88,74 @@ class Board
 
   def is_null_piece?(pos)
     self[pos].is_a?(NullPiece)
+  end
+
+  def is_opposing_piece?(pos,current_color)
+    return true if self[pos].color != current_color && !is_null_piece?(pos)
+    false
+  end
+
+  def in_check?(color)
+    #find the king of color
+    # find opposing pieces
+    # find possible moves of opposing Pieces
+    # if any position
+    king_pos = @all_pieces.select {|piece| piece.color == color &&
+      :king == piece.name}.first.position
+    opposing_pieces = @all_pieces.select {|piece| piece.color != color}
+    opposing_pieces.each do |piece|
+      # byebug if piece.name == :queen && piece.color == "black"
+      return true if piece.moves.include?(king_pos)
+    end
+    false
+  end
+
+  def checkmate?(color)
+    my_pieces = @all_pieces.select {|piece| piece.color == color}
+    my_pieces.each do |piece|
+      return false unless piece.valid_moves.empty?
+    end
+    true
+    # false
+  end
+
+  def deep_dup
+    new_board = self.dup
+    new_board.grid = self.grid.dup
+    new_board.grid.map!(&:dup)
+
+    new_pieces = @all_pieces.map(&:dup)
+
+    new_pieces.each do |piece|
+      piece.board = new_board
+      piece.position = piece.position.dup
+      new_board[piece.position] = piece
+    end
+
+    new_board.all_pieces = new_pieces
+
+    new_board
+
+  end
+
+  def move(piece, new_pos)
+    # raise "Move will put you in check!" unless piece.valid_moves.include?(new_pos)
+
+    possible_moves = piece.moves
+    if possible_moves.include?(new_pos)
+      take_piece(new_pos) if is_take_move?(new_pos)
+      self[piece.position], self[new_pos] = self[new_pos], self[piece.position]
+      piece.position = new_pos
+    end
+  end
+
+  def take_piece(new_pos)
+    self[new_pos] = NullPiece.instance
+  end
+
+  def is_take_move?(new_pos)
+    return true unless self.is_null_piece?(new_pos)
+    false
   end
 
 
