e37bb16c2c7f226e740a7207df255c60b6e3d583
Clapinton <eric.olivetree@gmail.com>
2016-11-15 11:03:31 -0800

    Moving in one direction is possible

--- 2048/lib/grid.js
+++ 2048/lib/grid.js
@@ -9,7 +9,8 @@ class Grid {
 
     this.setupGrid();
 
-    // POJO with wither undefined, false or a tile object
+    // POJO:
+    // this.filledPositions['24'] = TileObj
     this.filledPositions = {};
 
     // Spawns the first 2 tiles
@@ -28,7 +29,6 @@ class Grid {
       for (let cell = 1; cell < 5; cell++) {
         let $gridCell = $("<div class='grid-cell'>");
         $gridRow.append($gridCell);
-        this.filledPositions[this.posToString(row,cell)] = {};
       }
       $gridContainer.append($gridRow);
     }
@@ -44,16 +44,11 @@ class Grid {
 
     $(".tile-container").append(newTile.$html);
 
-    this.filledPositions[pos].tile = newTile;
-    this.filledPositions[pos].newPosition = pos;
+    this.filledPositions[pos] = newTile;
+    // this.filledPositions[pos].newPosition = pos;
 
     return newTile;
   }
-
-  moveTiles(direction) {
-    //direction can be "UP", "DOWN", "LEFT", "RIGHT"
-  }
-
 
   randomPosition() {
     let row, col;
@@ -68,11 +63,43 @@ class Grid {
   }
 
   positionTaken(r,c) {
-    return this.filledPositions[this.posToString(r,c)].tile;
+    return this.filledPositions[this.posToString(r,c)];
   }
 
   posToString(r,c) {
     return `${r}${c}`;
+  }
+
+
+  moveTiles(direction) {
+    let row, col, pos;
+    for (let cell = 0; cell < 16; cell++) {
+      row = Math.floor(cell%4) + 1;
+      col = Math.floor(cell/4) + 1;
+      pos = this.posToString(row,col);
+      console.log("checking @ ", pos);
+
+      // If there's NO tile on pos, look for the next tile to move it into the empty pos
+      if (!this.filledPositions[pos]) this.searchAndMove(row, col);
+
+    }
+    //direction can be "UP", "DOWN", "LEFT", "RIGHT"
+  }
+
+
+  searchAndMove(row, col) {
+    let pos = this.posToString(row,col);
+    // debugger
+    for (let oldRow = row+1; oldRow < 5; oldRow++) {
+      let oldPos = this.posToString(oldRow,col);
+      if (this.filledPositions[oldPos]) {
+        console.log("tile found at ", oldPos, this.filledPositions[oldPos]);
+        this.filledPositions[pos] = this.filledPositions[oldPos];
+        this.filledPositions[pos].setPosition(pos);
+        this.filledPositions[oldPos] = false;
+        return;
+      }
+    };
   }
 
 
