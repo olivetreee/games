76af2b9d069acbe556d0e97087f52bbb65b89435
Clapinton <eric.olivetree@gmail.com>
2016-11-18 01:16:41 -0800

    Change in tile HTML structure to allow for spawn animation

--- 2048/lib/tile.js
+++ 2048/lib/tile.js
@@ -7,7 +7,11 @@ function Tile (position, merged=false) {
   this.value = Math.round(Math.random()+1)*2;
   this.position = position;
 
-  this.$html = $(`<div class='tile val-${this.value} pos-${position}'>${this.value}</div>`);
+  this.$html = ""
+  this.$outerHtml = $(`<div class='tile-wrapper pos-${position}'></div>`);
+  this.$innerHtml = $(`<div class='tile val-${this.value}'>${this.value}</div>`);
+  this.mountHtml();
+
 }
 
 Tile.prototype.setPosition = function(newPos) {
@@ -22,8 +26,9 @@ Tile.prototype.doubleValue = function() {
 }
 
 Tile.prototype.setValue = function(val) {
-  this.$html.text(val);
-  this.$html.removeClass(`val-${this.value}`).addClass(`val-${val}`);
+  this.$innerHtml.text(val);
+  this.$innerHtml.removeClass(`val-${this.value}`).addClass(`val-${val}`);
+  this.mountHtml();
   this.value = val;
 }
 
@@ -33,20 +38,27 @@ Tile.prototype.removeSelf = function() {
   window.setTimeout(() => $(htmlClass).remove(), 200);
 }
 
+Tile.prototype.mountHtml = function() {
+  this.$html = this.$outerHtml.html(this.$innerHtml);
+}
+
 Tile.prototype.spawn = function() {
   // debugger
-  const tileClass = `pos-${this.position}`;
-  console.log(document.getElementsByClassName(tileClass));
-  const tileEl = document.getElementsByClassName(tileClass)[0];
+  const tileClass = `.pos-${this.position}`;
+  const tileEl = document.querySelector(`${tileClass} .tile`);
+  console.log(tileEl);
 
   tileEl.animate({
-    opacity: [0,1],
+    transform: ['scale(0,0)', 'scale(1,1)'],
   }, {
     // Apply effect during delay.
     fill: 'backwards',
 
+    // Wait some time while move happens
+    delay: 100,
+
     // Iterations last for 2000ms.
-    duration: 300,
+    duration: 200,
 
     // Play every second iteration backwards.
     direction: 'normal',
